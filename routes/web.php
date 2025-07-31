075adb101576f0c07732ed80750cb6b684d79bb7
feliperibeirosesi <felipejosenew@gmail.com>
2025-07-31 13:43:40 -0300

    salvei mas toma cuidado viu, qualquer coisa utilize as versões antigas

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,13 @@ use App\Http\Controllers\AuthController;
 Route::get('/', [AuthController::class, 'index']);
 
 Route::get('/singin', [AuthController::class, 'register']);
-
-Route::get('/auth/google', [AuthController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
+Route::get('/auth/google', [AuthController::class, 'redirectToGoogle']);
 // Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/test-session', function () {
+    session(['foo' => 'bar']);
+    return session('foo');
+});
+
 
