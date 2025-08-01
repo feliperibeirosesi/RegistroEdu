02359f3dd44ce7767878b1128060ae1601bb6826
feliperibeirosesi <felipejosenew@gmail.com>
2025-08-01 14:36:18 -0300

    Implementação de login por google funcionou

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GoogleController;
+use Illuminate\Support\Facades\Auth;
 
 
-Route::get('/', [AuthController::class, 'index']);
+Route::get('/', function () {
+    if(Auth::check()){
+        return view('page.HomeScreen');
+    }
+    return view('page.HomeScreen');
+});
 
-Route::get('/singin', [AuthController::class, 'register']);
-Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
-Route::get('/auth/google', [AuthController::class, 'redirectToGoogle']);
+
+
+Route::get('/singin', function(){
+    return view('page.RegisterScreen');
+});
+
+
+Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle']);
+Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+
 // Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/test-session', function () {
